1768c08a9040952cfe55d5316f7125e7bd21e87a
rh-abir <rhabir.co@gmail.com>
2023-05-19 22:54:47 +0600

    get data by id

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.as0kbdg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,10 +31,34 @@ async function run() {
       .db("animalToysDB")
       .collection("animalToy");
 
+
+
+    //  get all data 
     app.get('/allAnimalData', async(req, res) => {
       const result = await animalToyCollection.find().toArray()
       res.send(result)
     })
+
+    // get data by category 
+    app.get('/category/:name', async(req, res) => {
+      const categoryName = req.params.name;
+      const qurey = {category: categoryName}
+
+      const result = await animalToyCollection.find(qurey).toArray()
+      res.send(result)
+
+    })
+
+    // get data by id 
+    app.get('/toydetails/:id', async(req, res) => {
+      const id = req.params.id;
+      // console.log('hitting route', id)
+      const qurey = {_id: new ObjectId(id)}
+      const result = await animalToyCollection.findOne(qurey)
+      res.send(result)
+    })
+
+
 
     app.post("/animaltoy", async (req, res) => {
       const body = req.body;
